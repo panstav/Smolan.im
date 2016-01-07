833486e4d0fc7aacede363116fee2c1236896e39
Stav Pan Geffen <stavgeffen@gmail.com>
2016-01-07 22:14:11 +0200

    Fetcher: Added rate limiter to /run-fetcher route

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 'use strict';
 
 var express = require('express');
+var limiter = require('express-rate-limit');
 
 var fetch = require('./fetcher/index');
 
 // Start a new server, set it up and return it.
 module.exports.init = () => {
+
+	var rateLimiter;
+
+	if (process.env.LOCAL === 'true'){
+
+		// prep rate limiter
+		// every 30 minutes, allow only a single request
+		// than only send status 429 "Too Many Requests"
+		// skip on local instances
+		rateLimiter = limiter(
+			{
+				windowMs: 1000 * 60 * 30,
+				max: 1,
+				delayAfter: 0,
+				delayMs: 0
+			}
+		);
+
+	} else {
+		rateLimiter = (req, res, next) => { next(); };
+	}
 
 	// Boing
 	let server = express();
@@ -16,7 +38,7 @@ module.exports.init = () => {
 	});
 
 	// register fetcher job initiator
-	server.get('/run-fetcher', (req, res) => {
+	server.get('/run-fetcher', rateLimiter, (req, res) => {
 
 		fetch()
 			.then(() => { res.status(200).end(); })
