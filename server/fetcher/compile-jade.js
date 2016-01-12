ac2de399e073275fd834bc266ce177b1dcccc4e8
Stav Pan Geffen <stavgeffen@gmail.com>
2016-01-12 19:22:43 +0200

    Client: Added Bing Webmasters Verification

--- server/fetcher/compile-jade.js
+++ server/fetcher/compile-jade.js
@@ -15,7 +15,8 @@ module.exports = function(sortedHeadlines){
 			description: 'אוסף מאמרים מהאתרים: הארץ, המקום הכי חם בגהינום, העוקץ, הטלויזיה החברתית ו- שיחה מקומית. זכרו - כמו עם כל גוף ידע - השתמשו בביקורתיות ובתבונה כאשר אתם קוראים "אקטואליה".',
 			siteName: 'שמאלנים',
 			logoUrl: common.domain + '/logo.png',
-			categorizedDB: sortedHeadlines
+			categorizedDB: sortedHeadlines,
+			bingVerification: '66210C1BD52CB9DFEAF89E92C2D0C35C'
 		};
 
 		if (process.env.NODE_ENV === 'production') locals.production = true;
