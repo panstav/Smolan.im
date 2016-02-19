144e99d61bce2a60d5bb6c311f48a416b45afaef
Stav Pan Geffen <stavgeffen@gmail.com>
2016-02-19 02:24:33 +0200

    Compile: Adjust per-source limitation
    Set a limit on number of headlines per single source per time-category, instead of limiting per source

--- common.js
+++ common.js
@@ -2,6 +2,7 @@ module.exports = {
 	domain: process.env.LOCAL ? 'http://localhost:3000' : 'http://www.smolan.im',
 
 	itemsPerMagazine: 5,
+	maxPerMagazingForToday: 3,
 
 	momentDayFormat: 'DD/MM/YYYY',
 	momentHumanFormat: 'DD/MM'
--- server/compile/sort-headlines.js
+++ server/compile/sort-headlines.js
@@ -5,15 +5,18 @@ const getDomain = require('top-domain');
 
 const common = require('../../common');
 
-module.exports = latestHeadlines => {
+const thisVeryMoment = moment();
+
+const today = thisVeryMoment.format(common.momentDayFormat);
+const yesterday = thisVeryMoment.subtract(1, 'days').format(common.momentDayFormat);
+const sevenDaysAgo = thisVeryMoment.subtract(7, 'days');
+
+module.exports = (latestHeadlines) => {
 
 	var sortedHeadlines = latestHeadlines.map(toPlainObject)
 
 		// sort by descending date
 		.sort(byDate)
-
-		// limit per-magazine
-		.reduce(maxHeadlinesPerMagazine(), [])
 
 		// turn url into a redirection route, with url as query
 		// this is for tracking outbound links
@@ -40,63 +43,44 @@ function byDate(a, b){
 	return -1;
 }
 
-function maxHeadlinesPerMagazine(){
-
-	const counter = {};
-
-	return (arr, headline) => {
-
-		const domainName = getDomain(headline.url);
-
-		// if counter wasn't initiated for this domain, include it, increment and continue
-		if (!counter[domainName]){
-			counter[domainName] = 1;
-
-			arr.push(headline);
-			return arr;
-		}
-
-		// if it already reached max headlines, don't include it
-		if (counter[domainName] === common.itemsPerMagazine) return arr;
-
-		// otherwise we're just counting, include it and continue
-		counter[domainName]++;
-		arr.push(headline);
-		return arr;
-	};
-
-}
-
 function innerUrls(headline){
 	headline.url = common.domain + '/redirect?url=' + headline.url;
 
 	return headline;
 }
 
-function sortedByTimeCategory(accumulator, headline){
+function sortedByTimeCategory(accumulator, currentHeadline){
 
-	const thisVeryMoment = moment();
+	const targetTimeCategory = getTimeCatorgory();
 
-	const today = thisVeryMoment.format(common.momentDayFormat);
-	const yesterday = thisVeryMoment.subtract(1, 'days').format(common.momentDayFormat);
-	const sevenDaysAgo = thisVeryMoment.subtract(7, 'days');
-
-	const queryDate = moment(headline.date).format(common.momentDayFormat);
-
-	if (queryDate === today){ // was it today?
-		accumulator.today.push(headline);
-
-	} else if (queryDate === yesterday){ // was it yesterday?
-		accumulator.yesterday.push(headline);
-
-	} else if (sevenDaysAgo.isBefore(headline.date)){ // was during last seven days?
-		accumulator.lastSevenDays.push(headline);
-
-	} else { // either way - it's this week
-		accumulator.lastThirtyDays.push(headline);
-	}
+	if (sourceDidntBinge(targetTimeCategory)) accumulator[targetTimeCategory].push(currentHeadline);
 
 	return accumulator;
+
+	function getTimeCatorgory(){
+		const queryDate = moment(currentHeadline.date).format(common.momentDayFormat);
+
+		if (queryDate === today){ // was it today?
+			return 'today';
+
+		} else if (queryDate === yesterday){ // was it yesterday?
+			return 'yesterday';
+
+		} else if (sevenDaysAgo.isBefore(currentHeadline.date)){ // was during last seven days?
+			return 'lastSevenDays';
+
+		} else { // either way - it's this week
+			return 'lastThirtyDays';
+		}
+
+	}
+
+	function sourceDidntBinge(timeCategory){
+		const appearancesAtTimeCategory = accumulator[timeCategory].filter(headline => headline.source === currentHeadline.source);
+
+		return appearancesAtTimeCategory.length < common.maxPerMagazingForToday;
+	}
+
 }
 
 function dateToHuman(headline){
