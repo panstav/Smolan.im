c58f4f2db7887299ff42cb3578a7b326f209b7e1
Stav Geffen <stavgeffen@gmail.com>
2016-09-24 00:09:06 +0300

    fixup! added a cron job to crawl headlines every hour

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+require('./cron')();
 const server = require('./server');
-const registerCron = require('./cron');
 
 const port = process.env.PORT || 3000;
 server.init().listen(port, () => {
 	console.log(`Server is up! Listening on ${port}.`);
-	registerCron();
 });
